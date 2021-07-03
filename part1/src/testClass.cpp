c787322607d7a7eefe22f7dd6da86401a3a3f33d
antolamb <anton.lambrecht@ugent.be>
2021-07-03 14:51:18 +0200

    second commit

---
+++ part1/src/testClass.cpp
@@ -0,0 +1,5 @@
+#include "testClass.h"
+#include <iostream>
+void testClass::foo(){
+	std::cout<<"Dit is een test waarbij appart gedefinieerd wordt en geimplementeerd!\n";
+}
---
+++ part1_library/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "testClass.h"
+
+int main(int argc, char ** argv){
+	std::cout<<"hallo\n";
+	testClass().foo();
+	return 0;
+}
